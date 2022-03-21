1cf64cd2c8b30c9734bab3f30cdd62d341dc1386
WTDula <wtdula@gmail.com>
2022-03-21 11:55:06 -0500

    all necessary files imported, __init__ methods defined, other methods declared according to UML

---
+++ battlefield.py
@@ -0,0 +1,31 @@
+from fleet import Fleet
+from herd import Herd
+
+class Battlefield:
+    def __init__(self):
+        self.herd = None
+        self.fleet = None
+
+    def run_game(self):
+        pass
+
+    def display_welcome(self):
+        pass
+
+    def battle(self):
+        pass
+
+    def dino_turn(self, dino):
+        pass
+
+    def robo_turn(self, robo):
+        pass
+
+    def show_dino_opponent_options(self):
+        pass
+
+    def show_robo_opponent_options(self):
+        pass
+
+    def display_winners(self):
+        pass
---
+++ dinosaur.py
@@ -0,0 +1,8 @@
+class Dinosaur:
+    def __init__(self, name, attack_power):
+        self.name = name
+        self.health = 100
+        self.attack_power = attack_power
+
+    def attack(self, robo):
+        pass
