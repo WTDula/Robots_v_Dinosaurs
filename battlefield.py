9e5fed52e1cf04e9b6f0ec0dc90a925119334f5c
WTDula <wtdula@gmail.com>
2022-03-21 15:55:43 -0500

    added dino_turn, robo_turn, show_dino_opponent_options, show_robo_opponent_options, displays and calculations that showed up repetetively

--- battlefield.py
+++ battlefield.py
@@ -3,30 +3,90 @@ from herd import Herd
 
 class Battlefield:
     def __init__(self):
-        self.herd = None
-        self.fleet = None
+        self.herd = Herd()
+        self.fleet = Fleet()
 
     def run_game(self):
-        pass
+        self.herd.create_herd()
+        self.fleet.create_fleet()
+        self.battle()
 
     def display_welcome(self):
         print("Welcome to Robots Versus Dinosaurs!  Who will be the winner?")
         print("-------------------------------------------------------------------")
 
     def battle(self):
-        
+        dino_total_health = self.calc_total_dino_health()
+        robo_total_health = self.calc_total_robot_health()
+        while(dino_total_health > 0 or robo_total_health > 0):
+            self.display_dinosaurs()
+            user_input = input("Which dinosaur would you like to attack with? (1, 2, 3) ")
+            if(user_input == 1):
+                self.dino_turn(self.herd.dinosaurs[0])
+            elif(user_input == 2):
+                self.dino_turn(self.herd.dinosaurs[1])
+            else:
+                self.dino_turn(self.herd.dinosaurs[2])
+            self.display_robots()
+            user_input = input("Which robot would you like to attack with? (1, 2, 3) ")
+            if(user_input == 1):
+                self.robo_turn(self.fleet.robots[0])
+            elif(user_input == 2):
+                self.robo_turn(self.fleet.robots[1])
+            else:
+                self.robo_turn(self.fleet.robots[2])
+            dino_total_health = self.calc_total_dino_health()
+            robo_total_health = self.calc_total_robot_health()
+        self.display_winners()
+
 
     def dino_turn(self, dino):
-        pass
+        self.show_dino_opponent_options()
+        user_input = input("Which enemy do you wish to attack? (1, 2, 3) ")
+        if(user_input == 1):
+            dino.attack(self.fleet.robots[0])
+        elif(user_input == 2):
+            dino.attack(self.fleet.robots[1])
+        else:
+            dino.attack(self.fleet.robots[2])
 
     def robo_turn(self, robo):
-        pass
+        self.show_robo_opponent_options()
+        user_input = input("Which enemy would you lke to attack? (1, 2, 3) ")
+        if(user_input == 1):
+            robo.attack(self.herd.dinosaurs[0])
+        elif(user_input == 2):
+            robo.attack(self.herd.dinosaurs[1])
+        else:
+            robo.attack(self.herd.dinosaurs[2])
 
+    # I am the dino, show me the robos I can attack
     def show_dino_opponent_options(self):
-        pass
+        self.display_robots()
 
+
+    # I am the robo, show me the dinos I can attack
     def show_robo_opponent_options(self):
-        pass
+        self.display_dinosaurs()
+
 
     def display_winners(self):
         pass
+
+    def display_dinosaurs(self):
+        print(f" 1. {self.herd.dinosaurs[0].name}'s HP = {self.herd.dinosaurs[0].health} ")
+        print(f" 2. {self.herd.dinosaurs[1].name}'s HP = {self.herd.dinosaurs[1].health} ")
+        print(f" 3. {self.herd.dinosaurs[2].name}'s HP = {self.herd.dinosaurs[2].health} ")
+        print("-----------------------------------------------------------------------------")
+
+    def display_robots(self):
+        print(f" 1. {self.fleet.robots[0].name}'s HP = {self.fleet.robots[0].health} ")
+        print(f" 2. {self.fleet.robots[1].name}'s HP = {self.fleet.robots[1].health} ")
+        print(f" 3. {self.fleet.robots[2].name}'s HP = {self.fleet.robots[2].health} ")
+        print("-----------------------------------------------------------------------------")
+
+    def calc_total_dino_health(self):
+        return self.herd.dinosaurs[0].health + self.herd.dinosaurs[1].health + self.herd.dinosaurs[2].health
+
+    def calc_total_robot_health(self):
+        return self.fleet.robots[0].health + self.fleet.robots[1].health + self.fleet.robots[2].health
--- main.py
+++ main.py
@@ -1,13 +1,4 @@
 from battlefield import Battlefield
-from fleet import Fleet
-from herd import Herd
 
 battlefield_1 = Battlefield()
-
-#test create_fleet
-robo_fleet = Fleet()
-robo_fleet.create_fleet()
-
-#test ceate_herd
-dino_herd = Herd()
-dino_herd.create_herd()
+battlefield_1.run_game()
